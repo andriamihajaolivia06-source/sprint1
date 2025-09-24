a4e7a834fa4de5d78f19c66f0695aec769bd091a
andriamihajaolivia06-source <andriamihajaolivia06@gmail.com>
2025-09-24 09:53:09 +0300

    create FrontServlet

---
+++ framework/src/com/sprint1/FrontServlet.java
@@ -0,0 +1,48 @@
+package com.monframework;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class FrontServlet extends HttpServlet {
+
+   
+    private void servicePersonnalisee(HttpServletRequest request, HttpServletResponse response) 
+            throws ServletException, IOException {
+
+        String fullUri = request.getRequestURI();       
+        String contextPath = request.getContextPath(); 
+        String relativeUri = fullUri.substring(contextPath.length());  
+
+        if (relativeUri.startsWith("/")) {
+            relativeUri = relativeUri.substring(1);  
+        }
+
+        response.setContentType("text/html; charset=UTF-8");
+
+        try (PrintWriter out = response.getWriter()) {
+            out.println("<html>");
+            out.println("<head><title>FrontServlet</title></head>");
+            out.println("<body>");
+            out.println("<h1>URL saisie : " + relativeUri + "</h1>");
+            out.println("</body>");
+            out.println("</html>");
+        }
+    }
+
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) 
+            throws ServletException, IOException {
+        servicePersonnalisee(request, response);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) 
+            throws ServletException, IOException {
+        servicePersonnalisee(request, response);
+    }
+}
