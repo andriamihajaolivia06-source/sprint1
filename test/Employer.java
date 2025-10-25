f3988ada985f2658d50ce4891bee350b2290cab8
andriamihajaolivia06-source <andriamihajaolivia06@gmail.com>
2025-10-25 20:42:06 +0300

    modification

--- test/Employer.java
+++ test/Employer.java
@@ -1,11 +1,8 @@
 package com.sprint1;
 
-import jakarta.ws.rs.Path;
-
-
 public class Employer {
-    
-    @Path("/employer")
+
+    @PathAnnotation("/employer")
     public String getDetails() {
         return "Employer details";
     }
--- test/Main.java
+++ test/Main.java
@@ -1,28 +1,22 @@
 package com.sprint1;
 
 import java.lang.reflect.Method;
-import jakarta.ws.rs.Path;
 
 public class Main {
     public static void main(String[] args) {
-        try {
-            // Instancier Employer
+               try {
             Employer employer = new Employer();
-            
-            // Appeler la méthode
             String result = employer.getDetails();
             System.out.println("Method result: " + result);
-            
-            // Récupérer l'annotation @Path de la méthode
+
             Method method = Employer.class.getMethod("getDetails");
-            Path pathAnnotation = method.getAnnotation(Path.class);
-            if (pathAnnotation != null) {
-                System.out.println("URL from @Path: " + pathAnnotation.value());
+            PathAnnotation path = method.getAnnotation(PathAnnotation.class);
+            if (path != null) {
+                System.out.println("URL from @Path: " + path.value());
             } else {
                 System.out.println("No @Path annotation found on method");
             }
         } catch (Exception e) {
-            System.err.println("Error: " + e.getMessage());
             e.printStackTrace();
         }
     }
